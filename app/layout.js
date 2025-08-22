2ab1a2b4c7e3ac5a473048718dd1a6a686341517
Muhammad Hashir Awaiz <hashir.avaiz@gmail.com>
2025-08-22 17:02:14 +0500

    Corrected ES Lint Error for Deployment

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 // app/layout.js
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata = {
   title: "SwarmAid",
@@ -13,10 +14,10 @@ export default function RootLayout({ children }) {
         {/* Navbar */}
         <nav className="bg-gray backdrop-blur border-b border-gray-800 sticky top-0 z-20">
           <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
-            <a href="/" className="text-xl font-bold">SwarmAid</a>
+            <Link href="/" className="text-xl font-bold">SwarmAid</Link>
             <div className="flex gap-6 text-sm font-medium">
-              <a href="/" className="hover:text-blue-400">Dashboard</a>
-              <a href="/about" className="hover:text-blue-400">About Us</a>
+              <Link href="/" className="hover:text-blue-400">Dashboard</Link>
+              <Link href="/about" className="hover:text-blue-400">About Us</Link>
             </div>
           </div>
         </nav>
